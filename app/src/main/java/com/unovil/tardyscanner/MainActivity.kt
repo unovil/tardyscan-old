b2338dca4e89ff6ceb022471ff4237482a3e6837
Juan Miguel Villegas <70586029+unovil@users.noreply.github.com>
2023-08-19 22:21:03 +0800

    Feature: Sign-in and auth added

--- app/src/main/java/com/unovil/tardyscanner/MainActivity.kt
+++ app/src/main/java/com/unovil/tardyscanner/MainActivity.kt
@@ -2,31 +2,43 @@ package com.unovil.tardyscanner
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import com.unovil.tardyscanner.databinding.ActivityMainBinding
-import java.util.Properties
-
-lateinit var properties: Properties
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.gotrue.SessionStatus
+import io.github.jan.supabase.gotrue.gotrue
+import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
+import org.koin.core.qualifier.named
 
 /**
  * @author Juan Miguel L. Villegas
  */
 class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val supabaseUrl: String by inject(named("supabaseUrl"))
+    private val supabaseKey: String by inject(named("supabaseKey"))
+    private val client: SupabaseClient by inject(named("supabaseClient")) { parametersOf(supabaseUrl, supabaseKey) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        properties = Properties().apply {
-            resources.openRawResource(R.raw.server_link).use { inputStream ->
-                load(inputStream)
+        when (client.gotrue.sessionStatus.value) {
+            is SessionStatus.Authenticated -> {
+                proceedToActivity(ScannerActivity::class.java)
+            }
+            else -> {
+                Log.i("MainActivity", "Value of sessionStatus is ${client.gotrue.sessionStatus.value}")
+                proceedToActivity(SignInActivity::class.java)
             }
         }
+    }
 
-        val intent = Intent(this, ScannerActivity::class.java)
-        intent.putExtra("SECRET_KEY", properties.getProperty("SECRET_KEY"))
-        intent.putExtra("SUPABASE_URL", properties.getProperty("SUPABASE_URL"))
-        intent.putExtra("SUPABASE_KEY", properties.getProperty("SUPABASE_KEY"))
+    private fun <T: ComponentActivity> proceedToActivity(activity: Class<T>) {
+        val intent = Intent(this, activity)
         startActivity(intent)
     }
 }
